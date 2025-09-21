0635829febb0fce8eda54728ad272d8908fabeab
Nada Etman <nadae6912@gmail.com>
2025-09-22 01:04:17 +0300

    Update

--- src/CartAction/clearCart.ts
+++ src/CartAction/clearCart.ts
@@ -2,21 +2,21 @@
 
 import { getMyToken } from "@/utilities/token"
 import axios from "axios"
-import { headers } from "next/headers"
 
- export async function clearCartAction(id: string, count:number){
-const token = await getMyToken()
-if(!token){
+export async function clearCartAction() {
+  const token = await getMyToken()
+  if (!token) {
     throw Error("Login First")
-}
-const value = {
-    count: count
-}
-const {data} = await axios.delete(`https://ecommerce.routemisr.com/api/v1/cart` ,{ 
-    headers:{
-        token : token as string
-    }
- })
- return data
+  }
 
+  const { data } = await axios.delete(
+    `https://ecommerce.routemisr.com/api/v1/cart`,
+    {
+      headers: {
+        token: token as string,
+      },
+    }
+  )
+
+  return data
 }
