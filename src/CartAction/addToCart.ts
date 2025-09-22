5aaa09a744080ee49359523da60b0870468d9875
Nada Etman <nadae6912@gmail.com>
2025-09-22 12:55:20 +0300

    update

--- src/CartAction/addToCart.ts
+++ src/CartAction/addToCart.ts
@@ -1,27 +1,22 @@
-"use server"
 import { getMyToken } from "@/utilities/token";
 import axios from "axios";
 
 export async function AddToCartAction(id: string) {
   try {
     const token = await getMyToken();
-
     if (!token) {
       throw new Error("No token found, user not logged in");
     }
 
-    const values = {
-      productId: id,
-    };
+    const values = { productId: id };
 
     const { data } = await axios.post(
       "https://ecommerce.routemisr.com/api/v1/cart",
       values,
       {
-      headers: {
-  Authorization: `Bearer ${String(token)}`,
-},
-
+        headers: {
+          token: String(token),   // ✅ موحد مع getUserCartAction
+        },
       }
     );
 
