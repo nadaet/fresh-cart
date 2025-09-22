6c22b2c249956fa91dbcb303c511c1c3afdfc9e1
Nada Etman <nadae6912@gmail.com>
2025-09-22 16:04:20 +0300

    update

--- src/CartAction/getUserCart.ts
+++ src/CartAction/getUserCart.ts
@@ -1,13 +1,21 @@
+"use server"
 import { getMyToken } from "@/utilities/token"
 import { Cart } from "@/types/cart.type"
 
-export async function getUserCartAction(): Promise<Cart> {
+export async function getUserCartAction(): Promise<Cart | null> {
   const token = await getMyToken()
-  if (!token) throw new Error("No token found")
+  if (!token) return null
 
   const response = await fetch("https://ecommerce.routemisr.com/api/v1/cart", {
-    headers: { token }
+    headers: {
+      token: token,
+    },
   })
+
+  if (!response.ok) {
+    throw new Error("Unexpected response from server")
+  }
+
   const data: Cart = await response.json()
   return data
 }
