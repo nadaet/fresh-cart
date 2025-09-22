1f6cd7cecca143a991a267c0177b1175cf199020
Nada Etman <nadae6912@gmail.com>
2025-09-22 14:16:04 +0300

    update

--- src/app/productDetails/[id]/page.tsx
+++ src/app/productDetails/[id]/page.tsx
@@ -10,6 +10,8 @@ const ProductDetails = async ({ params }: { params: Promise<{ id: string }> }) =
 
   return (
     <div className="w-full px-5 md:w-[80%] md:px-0 mx-auto my-10 flex items-center flex-col md:flex-row">
+      
+      {/* صورة المنتج */}
       <div className="w-full md:w-1/3">
         <Image
           width={500}
@@ -20,6 +22,7 @@ const ProductDetails = async ({ params }: { params: Promise<{ id: string }> }) =
         />
       </div>
 
+      {/* تفاصيل المنتج */}
       <div className="w-full md:w-2/3 m-10 md:m-0 ps-10">
         <h2 className="text-4xl font-bold">{data.title}</h2>
         <p className="mt-5">{data.description}</p>
@@ -33,7 +36,8 @@ const ProductDetails = async ({ params }: { params: Promise<{ id: string }> }) =
           </div>
         </div>
 
-<AddBtnCart productId={data.id} />
+        {/* زر إضافة الكارت */}
+        <AddBtnCart productId={data._id} />
       </div>
     </div>
   )
