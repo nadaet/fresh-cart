f1d0fc7f24066651c1a25fff5aa0af3f43c324f4
Nada Etman <nadae6912@gmail.com>
2025-09-22 14:10:39 +0300

    update

--- src/app/_componentes/navbar/AddBtnCart/AddBtnCart.tsx
+++ src/app/_componentes/navbar/AddBtnCart/AddBtnCart.tsx
@@ -3,6 +3,7 @@
 import React, { useContext } from "react"
 import { cartContext } from "@/Context/CartContext"
 import { Button } from "@/components/ui/button"
+import { toast } from "sonner"
 
 export default function AddBtnCart({ productId }: { productId: string }) {
   const { addProductToCart } = useContext(cartContext) 
@@ -10,8 +11,10 @@ export default function AddBtnCart({ productId }: { productId: string }) {
   const handleAdd = async () => {
     try {
       await addProductToCart(productId)
+      toast.success("Product added to cart", { duration: 1000, position: "top-center" })
     } catch (error) {
       console.error(error)
+      toast.error("Failed to add product", { duration: 1000, position: "top-center" })
     }
   }
 
